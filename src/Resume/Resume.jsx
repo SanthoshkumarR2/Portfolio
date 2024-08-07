90d1fa40bdc6c51232cbc15f1a3b0ed24bb66c81
Santhoshkumar <santhoshkumarstudy@gmail.com>
2024-08-07 10:46:53 +0530

    Add

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Resume() {
-  const resumeLink = 'https://drive.google.com/file/d/1jWb-NkeK7XvCk6GX5dlx92-y60yNfLT3/view?usp=drive_link'; // Replace with your actual resume link
+  const resumeLink = 'https://drive.google.com/file/d/1kCFvnHMSVMMCIlJ1QknhzTxE8GOJUbVW/view?usp=sharing'; // Replace with your actual resume link
 
   const handleDownloadClick = () => {
     window.open(resumeLink, '_blank');
